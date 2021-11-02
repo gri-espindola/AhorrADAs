ef61a254366f76df9cba076c7ca80f71c06b7618
gri-espindola <griespindola@hotmail.com>
2021-11-01 21:50:32 -0300

    llamando los botones y secciones del DOM

--- ahorrAdas.js
+++ ahorrAdas.js
@@ -1,1 +1,12 @@
 //guardando cambios de la rama para crear funcionalidades para las secciones que me permitan cambiarme de modal entre categoria, balance y reporte
+
+//DOM
+
+//botones
+const botonBalance = document.querySelector("#boton-balance")
+const botonCategorias = document.querySelector("#boton-categorias")
+const botonReportes = document.querySelector("#boton-reportes")
+//secciones
+const seccionBalance = document.querySelector("#seccion-balance")
+const seccionCategorias = document.querySelector("#seccion-categorias")
+const seccionReportes = document.querySelector("#seccion-reportes")
