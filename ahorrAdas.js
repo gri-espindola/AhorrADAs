7e59f59aeb48e7d3d70fc786d4a36b156df27427
gri-espindola <griespindola@hotmail.com>
2021-11-03 22:14:12 -0300

    funcionalidad de botones y secciones hechas

--- ahorrAdas.js
+++ ahorrAdas.js
@@ -3,10 +3,39 @@
 //DOM
 
 //botones
-const botonBalance = document.querySelector("#boton-balance")
 const botonCategorias = document.querySelector("#boton-categorias")
+const botonBalance = document.querySelector("#boton-balance")
 const botonReportes = document.querySelector("#boton-reportes")
 //secciones
-const seccionBalance = document.querySelector("#seccion-balance")
 const seccionCategorias = document.querySelector("#seccion-categorias")
+const seccionBalance = document.querySelector("#seccion-balance")
 const seccionReportes = document.querySelector("#seccion-reportes")
+
+//Darle a las 3 display flex
+    seccionCategorias.style.display = 'none'
+    seccionBalance.style.display = 'flex'
+    seccionReportes.style.display = 'none'
+    
+
+botonCategorias.addEventListener('click', () =>{
+    seccionCategorias.style.display = 'flex'
+    seccionBalance.style.display = 'none'
+    seccionReportes.style.display = 'none'
+    
+});
+
+
+botonBalance.addEventListener('click', () =>{
+    seccionBalance.style.display = 'flex'
+    seccionCategorias.style.display = 'none'
+    seccionReportes.style.display = 'none'
+    
+});
+
+
+botonReportes.addEventListener('click', () =>{
+    seccionReportes.style.display = 'flex'
+    seccionBalance.style.display = 'none'
+    seccionCategorias.style.display = 'none'
+    
+});
