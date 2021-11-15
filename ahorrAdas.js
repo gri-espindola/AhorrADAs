8809dcddcffe5315527922143756890bff0f753c
Pamela <pamelafumagalli85@gmail.com>
2021-11-14 22:13:48 -0300

    Aceptar cambios

--- ahorrAdas.js
+++ ahorrAdas.js
@@ -84,17 +84,10 @@ agregarNuevaOperacion.addEventListener('click', () => {
 
 //modalOperacionDetallada.style.display = 'none'
 
-<<<<<<< HEAD
 botonAgregar.addEventListener('click', () => {
     modalOperacionDetallada.style.display = 'flex'
     modalNuevaOperacion.style.display = 'none'
     seccionOperaciones.style.display = 'flex'
-=======
-botonAgregar.addEventListener('click', () =>{
-    modalOperacionDetallada.style.display ='flex'
-    modalNuevaOperacion.style.display = 'none'
-    seccionOperaciones.style.display='none'
->>>>>>> 0e28028c0a5a55ad38afb490c1e3eacc7192be79
 
 })
 
